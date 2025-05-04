9f69817b52ed790940fde1f5c143479eba3d0fb5
ManuelWestermeier <westermeier111@gmail.com>
2025-05-04 11:04:08 +0200

    some server changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+const port = process.env.PORT || 3000;
+
 require("http").createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-
     try {
-        const url = new URL(req.url, `http://localhsot:80`);
+        const url = new URL(req.url, `http://localhost:${port}`);
         const query = url.searchParams.get("q");
         res.end("This is the response for " + query);
     } catch (error) {
         res.end("ERROR");
     }
-}).listen(80);
+}).listen(port);
